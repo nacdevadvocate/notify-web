6c52c3dfc90766da7184eeb2c7c1a3bb0c340f28
Eyvazov Farid <farid.eyvazov@nokia.com>
2024-12-03 18:02:48 +0100

    ecosystem file added for pm2 and logs will be hidden in production

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const env = import.meta.env.VITE_NODE_ENV;
+
+// replace console.* for disable log on production
+if (env === "production") {
+  console.log = () => { };
+  console.error = () => { };
+  console.debug = () => { };
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
